63032e83d5944e8ab4e0ddb67c7695b4582d750b
Patrick <patrykrzucidlo1@gmail.com>
2021-02-04 21:42:23 +0000

    Add readAll method to AbstractMetadataReader

--- lib/metadata-readers/abstract-metadata.reader.ts
+++ lib/metadata-readers/abstract-metadata.reader.ts
@@ -4,6 +4,16 @@ import { injectable } from 'inversify';
 export abstract class AbstractMetadataReader {
   public abstract read<T extends object, U extends keyof T>(object: U): any;
 
+  public readAll<T extends object, U extends keyof T>(objects: U[]): any {
+    let metadata: any[] = [];
+
+    for (const object of objects) {
+      metadata.push(object);
+    }
+
+    return metadata;
+  }
+
   protected isFunction(member: any): boolean {
     return typeof member === 'function';
   }
