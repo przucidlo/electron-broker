a2a13f3a571954811f9adb29a7b6254dd50f6e20
Patrick <patrykrzucidlo1@gmail.com>
2021-02-05 20:28:26 +0000

    Create ControllersMetadataFactoryComposer

---
+++ lib/container/composers/factory/controllers-metadata-factory.composer.ts
@@ -0,0 +1,40 @@
+import { Container } from 'inversify';
+import { Symbols } from '../../../constants/symbols';
+import { ControllerMetadata } from '../../../interfaces/controller-metadata.interface';
+import { Controller } from '../../../interfaces/controller.type';
+import { AbstractMetadataReader } from '../../../metadata-readers/abstract-metadata.reader';
+import { ContainerConfiguarableComposer } from '../../abstract/container-configurable-composer';
+
+export class ControllersMetadataFactoryComposer extends ContainerConfiguarableComposer {
+  private metadataReader: AbstractMetadataReader;
+
+  constructor(container: Container) {
+    super(container);
+
+    this.metadataReader = container.get(Symbols.ControllerMetadataReader);
+  }
+
+  public compose(): void {
+    this.container.bind(Symbols.ControllerMetadata).toFactory(() => {
+      return () => this.getControllersMetadata(this.getControllers());
+    });
+  }
+
+  private getControllers(): Record<string, unknown>[] {
+    return this.getControllersInstances(this.config.controllers);
+  }
+
+  private getControllersInstances(controllers: Controller[]): Record<string, unknown>[] {
+    return controllers.map((controller) => {
+      if (typeof controller === 'object' && controller !== null) {
+        return controller;
+      }
+
+      return this.container.get<any>(controller as any);
+    });
+  }
+
+  private getControllersMetadata(controllers: Record<string, unknown>[]): ControllerMetadata[] {
+    return controllers.map((controller) => this.metadataReader.read(controller as never));
+  }
+}
