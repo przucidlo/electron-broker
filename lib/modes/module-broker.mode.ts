6d3df3fe77b52949e8b44dfa9be099ab54ae64dd
Patrick <patrykrzucidlo1@gmail.com>
2021-02-07 15:47:17 +0000

    Implement EventDistributorAdapterRegistrator in ModuleBrokerMode

--- lib/modes/module-broker.mode.ts
+++ lib/modes/module-broker.mode.ts
@@ -1,59 +1,14 @@
-import { injectable, multiInject } from 'inversify';
-import { BrokerProcessAdapter } from '../adapters/broker-process.adapter';
-import { BrokerRendererAdapter } from '../adapters/broker-renderer.adapter';
-import { BROKER_EVENT } from '../constants/constants';
-import { Symbols } from '../constants/symbols';
-import { BrokerEventData } from '../interfaces/broker-event-data.interface';
-import { IpcTransport } from '../interfaces/ipc-transport.interface';
+import { inject, injectable } from 'inversify';
+import { EventDistributorAdapterRegistrator } from '../event-distributor/event-distributor-adapter-registator';
 import { ModuleMode } from '../interfaces/module-mode.interface';
 
 @injectable()
 export class ModuleBrokerMode implements ModuleMode {
-  private processAdapters: BrokerProcessAdapter[] = [];
-  private rendererAdapter: BrokerRendererAdapter;
-
-  constructor(@multiInject(Symbols.BrokerIpcTransport) adapters: IpcTransport[]) {
-    this.sortAdapters(adapters);
-  }
-
-  private sortAdapters(adapters: IpcTransport[]): void {
-    for (const adapter of adapters) {
-      if (adapter instanceof BrokerProcessAdapter) {
-        this.processAdapters.push(adapter);
-      }
-      if (adapter instanceof BrokerRendererAdapter) {
-        this.rendererAdapter = adapter;
-      }
-    }
-  }
+  constructor(
+    @inject(EventDistributorAdapterRegistrator) private eventDistributorRegistrator: EventDistributorAdapterRegistrator,
+  ) {}
 
   public start(): void {
-    this.registerProcessAdapters();
-    this.registerRendererAdapters();
-  }
-
-  private registerProcessAdapters(): void {
-    for (const adapter of this.processAdapters) {
-      adapter.register(BROKER_EVENT, (data: BrokerEventData) => {
-        this.distributeEvent(data);
-      });
-    }
-  }
-
-  private registerRendererAdapters(): void {
-    this.rendererAdapter.register(BROKER_EVENT, (event: any, data: BrokerEventData) => {
-      this.distributeEvent(data);
-    });
-  }
-
-  private distributeEvent(brokerEvent: BrokerEventData): void {
-    for (const adapter of this.processAdapters) {
-      // console.log('-----------In ModuleBroker:-----------');
-      // console.log(brokerEvent);
-      // console.log('--------------------------------------');
-
-      adapter.send(brokerEvent.pattern, brokerEvent);
-    }
-    this.rendererAdapter.send(brokerEvent.pattern, brokerEvent);
+    this.eventDistributorRegistrator.register();
   }
 }
