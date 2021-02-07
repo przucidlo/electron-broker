cd2d829b11645a513090a74f5bee0e5d71520c86
Patrick <patrykrzucidlo1@gmail.com>
2021-02-07 18:25:54 +0000

    Enable usage of controllers in BrokerMode

--- lib/modes/module-broker.mode.ts
+++ lib/modes/module-broker.mode.ts
@@ -1,14 +1,21 @@
 import { inject, injectable } from 'inversify';
+import { ControllersRegistrator } from '../controllers/controllers-registrator';
 import { EventDistributorAdapterRegistrator } from '../event-distributor/event-distributor-adapter-registator';
 import { ModuleMode } from '../interfaces/module-mode.interface';
+import { ModuleBaseMode } from './module-base.mode';
 
 @injectable()
-export class ModuleBrokerMode implements ModuleMode {
+export class ModuleBrokerMode extends ModuleBaseMode {
   constructor(
     @inject(EventDistributorAdapterRegistrator) private eventDistributorRegistrator: EventDistributorAdapterRegistrator,
-  ) {}
+    @inject(ControllersRegistrator) controllersRegistrator: ControllersRegistrator,
+  ) {
+    super(controllersRegistrator);
+  }
 
   public start(): void {
+    super.start();
+
     this.eventDistributorRegistrator.register();
   }
 }
