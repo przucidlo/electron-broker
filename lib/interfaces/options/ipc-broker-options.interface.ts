a91256c841a7a5416e430b5e7f66b854d7c0b3de
Patrick <patrykrzucidlo1@gmail.com>
2021-02-08 19:31:10 +0000

    Remove render property from IpcBrokerOptions

--- lib/interfaces/options/ipc-broker-options.interface.ts
+++ lib/interfaces/options/ipc-broker-options.interface.ts
@@ -1,8 +1,6 @@
 import { ChildProcess } from 'child_process';
-import { IpcRenderer } from 'electron';
 
 export interface IpcBrokerOptions {
   processes: ChildProcess[];
-  renderer: IpcRenderer;
   rendererSend: (pattern: any, data: any) => void;
 }
