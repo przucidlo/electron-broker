2c066bb550a21dbefdbf4ea99144a5422258dcb9
Patrick <patrykrzucidlo1@gmail.com>
2021-04-05 11:45:15 +0200

    Extract channels storage from IpcProcessCommunicator to new class IpcProcessChannels

--- lib/process/IpcProcessCommunicator.ts
+++ lib/process/IpcProcessCommunicator.ts
@@ -1,22 +1,11 @@
 import ChildProcess from 'child_process';
 import { IpcProcessPayload } from './IpcProcessPayload';
 import { v4 as uuid } from 'uuid';
-export type Listener = (...args: any[]) => unknown;
-
-/**
- * Describes how channels are stored inside of IpcProcessReceiver.
- */
-interface ChannelCollection {
-  [channelName: string]: Listener;
-}
-
-/**
- * Reusuable module that allows bidirectional communication
- * between fork process and It's father by usage of process ipc.
- */
+import { IpcProcessChannels } from './ipc-process-channels';
+import { MessageHandler } from '../types/message-handler.type';
 export default class IpcProcessCommunicator {
-  protected readonly process: NodeJS.Process | ChildProcess.ChildProcess;
-  private channels: ChannelCollection = {};
+  private readonly process: NodeJS.Process | ChildProcess.ChildProcess;
+  private readonly channels: IpcProcessChannels;
 
   constructor(childProcess?: NodeJS.Process | ChildProcess.ChildProcess) {
     this.process = childProcess ? childProcess : process;
@@ -32,39 +21,11 @@ export default class IpcProcessCommunicator {
     });
   }
 
-  public send(channelName: string, payload: unknown): void {
-    this.process.send(<IpcProcessPayload>{ messageId: uuid(), channelName: channelName, payload: payload }, (error) => {
-      if (error) {
-        console.error(error);
-      }
-    });
-  }
-
-  public on(channel: string, listener: Listener): void {
-    if (this.isChannelFree(channel)) {
-      this.addChannel(channel, listener);
-    }
-  }
-
-  private isChannelFree(channel: string): boolean {
-    const channelListener: Listener | null = this.findChannelListener(channel);
-
-    if (!channelListener) {
-      return true;
-    }
-    throw new Error('Handler for channel ' + channel + ' already exists.');
-  }
-
-  private addChannel(channel: string, listener: Listener): void {
-    this.channels = { ...this.channels, [channel]: listener };
-  }
-
   private async forwardToChannel(message: IpcProcessPayload): Promise<void> {
-    const channelListener = this.findChannelListener(message.channelName);
-    let result: any;
+    const channelListener = this.channels.getChannelListenerByName(message.channelName);
 
     if (channelListener) {
-      result = await Promise.resolve(channelListener(message.payload));
+      const result = await Promise.resolve(channelListener(message.payload));
 
       // Setting the channelName to undefined to make sure
       // the message won't be forwarded to any channel.
@@ -77,13 +38,19 @@ export default class IpcProcessCommunicator {
     this.process.send(payload);
   }
 
-  private findChannelListener(channelName: string): Listener | null {
-    for (const key of Object.keys(this.channels)) {
-      if (key === channelName) {
-        return this.channels[key];
+  public send(channelName: string, payload: unknown): void {
+    this.process.send(<IpcProcessPayload>{ messageId: uuid(), channelName: channelName, payload: payload }, (error) => {
+      if (error) {
+        console.error(error);
       }
-    }
+    });
+  }
 
-    return null;
+  public on(channel: string, listener: MessageHandler): void {
+    if (this.channels.isChannelFree(channel)) {
+      this.channels.addChannel(channel, listener);
+    } else {
+      throw new Error(`Listener for channel "${channel}" already exists.`);
+    }
   }
 }
