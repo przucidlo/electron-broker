c074af88fc5cc7a4f5d0f9ad790da9942158da49
Patrick <patrykrzucidlo1@gmail.com>
2021-04-05 13:17:36 +0200

    Write unit tests for IpcProcess class

---
+++ test/unit/process/ipc-process.spec.ts
@@ -0,0 +1,69 @@
+import IpcProcess from '../../../lib/process/ipc-process';
+import { mockProcess } from './mocks/mock-process';
+
+describe('IpcProcess', () => {
+  const mockChannelName = 'mock-channel';
+  const mockPayload = { test: 'test' };
+
+  let process: NodeJS.Process;
+  let ipcProcess: IpcProcess;
+
+  beforeEach(() => {
+    process = mockProcess();
+    ipcProcess = new IpcProcess(process);
+  });
+
+  describe('constructor', () => {
+    it('Should start listening for messages from main process', () => {
+      expect(process.on).toBeCalledWith('message', expect.any(Function));
+    });
+  });
+
+  describe('send', () => {
+    const consoleSpy = jest.spyOn(console, 'error').mockImplementation();
+
+    beforeEach(() => {
+      consoleSpy.mockClear();
+    });
+
+    it('Should use process.send method, to send message to other process', () => {
+      ipcProcess.send(mockChannelName, mockPayload);
+
+      expect(process.send).toBeCalledWith(
+        expect.objectContaining({
+          messageId: expect.any(String),
+          channelName: mockChannelName,
+          payload: mockPayload,
+        }),
+        expect.any(Function),
+      );
+    });
+
+    it('Should call console.error If any error occurs', () => {
+      ipcProcess.send(mockChannelName, mockPayload);
+
+      const errorHandler = (<jest.Mock>process.send).mock.calls[0][1];
+      errorHandler(new Error('Mock Error'));
+
+      expect(consoleSpy).toBeCalled();
+    });
+
+    afterAll(() => {
+      consoleSpy.mockRestore();
+    });
+  });
+
+  describe('on', () => {
+    const mockListener = () => ({});
+
+    it('Should register new channel', () => {
+      expect(() => ipcProcess.on(mockChannelName, mockListener)).not.toThrow();
+    });
+
+    it('Should throw error if channel already exists', () => {
+      ipcProcess.on(mockChannelName, mockListener);
+
+      expect(() => ipcProcess.on(mockChannelName, mockListener)).toThrowError();
+    });
+  });
+});
