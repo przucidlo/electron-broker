9494a9d8a042f311c7c0eb8b8bd85cdf569c7ba3
Patrick <patrykrzucidlo1@gmail.com>
2021-04-05 20:21:51 +0200

    Implement HandlerParamMetadata in createParamDecorator

--- lib/decorators/create-param.decorator.ts
+++ lib/decorators/create-param.decorator.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { HANDLER_ARGS_METADATA } from '../constants/decorators';
 import { BrokerEventData } from '../interfaces/broker-event-data.interface';
+import { HandlerParamMetadata } from '../interfaces/handler-param-metadata.interface';
 
 export function createParamDecorator<T>(method: (options: T, eventData: BrokerEventData) => any) {
   return (options?: T): ParameterDecorator => {
@@ -8,7 +9,14 @@ export function createParamDecorator<T>(method: (options: T, eventData: BrokerEv
       const args: any[] = Reflect.getMetadata(HANDLER_ARGS_METADATA, target, propertyKey) || [];
       const type = Reflect.getMetadata('design:paramtypes', target, propertyKey);
 
-      args.push({ index: index, method: method, options: options, type: type[index] });
+      const paramMetadata: HandlerParamMetadata<T> = {
+        index: index,
+        method: method,
+        options: options,
+        type: type[index],
+      };
+
+      args.push(paramMetadata);
 
       Reflect.defineMetadata(HANDLER_ARGS_METADATA, args, target, propertyKey);
     };
