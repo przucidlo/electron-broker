bf6e9dbfc197aee4fa9c80e42ccc01c2e32dc914
Patrick <patrykrzucidlo1@gmail.com>
2021-04-07 12:08:14 +0200

    Fix incorrect types for AbstractMetadataReader read and readAll functions

--- lib/metadata-readers/abstract-metadata.reader.ts
+++ lib/metadata-readers/abstract-metadata.reader.ts
@@ -2,9 +2,9 @@ import { injectable } from 'inversify';
 
 @injectable()
 export abstract class AbstractMetadataReader {
-  public abstract read<T extends Record<string, unknown>>(object: T): any;
+  public abstract read<T extends Record<any, unknown>, U extends keyof T>(object: U): any;
 
-  public readAll<T extends Record<string, unknown>, U extends keyof T>(objects: U[]): any {
+  public readAll<T extends Record<any, unknown>, U extends keyof T>(objects: U[]): any {
     const metadata: any[] = [];
 
     for (const object of objects) {
