d5c9dc584160c9fa5d2bd8205506e479cdafa423
Patrick <patrykrzucidlo1@gmail.com>
2021-04-07 12:10:13 +0200

    Extract reading of handlers metadata from ControllerMetadataReader to new class ControllerHandlersMetadataReader

---
+++ lib/metadata-readers/controller-handlers-metadata.reader.ts
@@ -0,0 +1,54 @@
+import 'reflect-metadata';
+import { injectable } from 'inversify';
+import { PATTERN_METADATA, HANDLER_ARGS_METADATA } from '../constants/decorators';
+import { ControllerHandlerMetadata } from '../interfaces/controller-handler-metadata.interface';
+import { MessageHandler } from '../types/message-handler.type';
+import { AbstractMetadataReader } from './abstract-metadata.reader';
+
+@injectable()
+export class ControllerHandlersMetadataReader extends AbstractMetadataReader {
+  public read<T extends Record<any, unknown>, U extends keyof T>(
+    controller: U,
+  ): Record<string, ControllerHandlerMetadata> {
+    const messageHandlers: Record<string, ControllerHandlerMetadata> = {};
+
+    for (const handlerName of this.getControllerProperties(controller)) {
+      const messageHandler = (controller as any)[handlerName].bind(controller);
+
+      if (this.isReadable(messageHandler, handlerName)) {
+        const pattern = this.getHandlerPattern(controller, handlerName);
+
+        if (this.isMessageHandler(pattern)) {
+          const paramsMetadata = this.getHandlerParamsMetadata(controller, handlerName);
+
+          messageHandlers[pattern] = {
+            handler: messageHandler,
+            paramsMetadata: paramsMetadata,
+          };
+        }
+      }
+    }
+
+    return messageHandlers;
+  }
+
+  private getControllerProperties(controller: unknown): string[] {
+    return Object.getOwnPropertyNames(Object.getPrototypeOf(controller));
+  }
+
+  private isReadable(handler: MessageHandler, handlerName: string): boolean {
+    return this.isFunction(handler) && handlerName !== 'constructor';
+  }
+
+  private getHandlerPattern(controller: unknown, handlerName: string) {
+    return Reflect.getMetadata(PATTERN_METADATA, controller, handlerName);
+  }
+
+  private getHandlerParamsMetadata(controller: unknown, handlerName: string) {
+    return Reflect.getMetadata(HANDLER_ARGS_METADATA, controller, handlerName);
+  }
+
+  private isMessageHandler(pattern: string): boolean {
+    return pattern !== undefined;
+  }
+}
--- lib/metadata-readers/controller-metadata.reader.ts
+++ lib/metadata-readers/controller-metadata.reader.ts
@@ -1,37 +1,22 @@
 import 'reflect-metadata';
 import { injectable } from 'inversify';
-import { HANDLER_ARGS_METADATA, PATTERN_METADATA } from '../constants/decorators';
 import { ControllerMetadata } from '../interfaces/controller-metadata.interface';
 import { AbstractMetadataReader } from './abstract-metadata.reader';
+import { ControllerHandlersMetadataReader } from './controller-handlers-metadata.reader';
 
 @injectable()
 export class ControllerMetadataReader extends AbstractMetadataReader {
-  public read<T extends Record<string, unknown>>(controller: T): ControllerMetadata {
+  constructor(private handlersMetadataReader: ControllerHandlersMetadataReader) {
+    super();
+  }
+
+  public read<T extends Record<any, unknown>>(controller: T): ControllerMetadata {
     const controllerMetadata: ControllerMetadata = {
       messageHandlers: {},
     };
 
-    for (const propertyKey of Object.getOwnPropertyNames(Object.getPrototypeOf(controller))) {
-      const messageHandler = (controller as any)[propertyKey].bind(controller);
-
-      if (this.isFunction(messageHandler) && propertyKey !== 'constructor') {
-        const pattern = Reflect.getMetadata(PATTERN_METADATA, controller, propertyKey);
-
-        if (this.isMessageHandler(pattern)) {
-          const handlerParamMetadata = Reflect.getMetadata(HANDLER_ARGS_METADATA, controller, propertyKey);
-
-          controllerMetadata.messageHandlers[pattern] = {
-            handler: messageHandler,
-            paramsMetadata: handlerParamMetadata,
-          };
-        }
-      }
-    }
+    controllerMetadata.messageHandlers = this.handlersMetadataReader.read(controller);
 
     return controllerMetadata;
-  }
-
-  private isMessageHandler(pattern: string): boolean {
-    return pattern !== undefined;
   }
 }
