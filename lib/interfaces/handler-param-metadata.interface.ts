596539b3a450b0c9c4bdac4fdc64b74f9ed83c31
Patrick <patrykrzucidlo1@gmail.com>
2021-04-07 12:07:12 +0200

    Add isHandlerParamMetadata type-guard

--- lib/interfaces/handler-param-metadata.interface.ts
+++ lib/interfaces/handler-param-metadata.interface.ts
@@ -6,3 +6,8 @@ export interface HandlerParamMetadata<T extends unknown> {
   type: () => unknown;
   method: (options: T, eventData: BrokerEventData) => void;
 }
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function isHandlerParamMetadata(arg: any): arg is HandlerParamMetadata<any> {
+  return typeof arg.index === 'number' && typeof arg.type === 'function' && typeof arg.method === 'function';
+}
