4478aab94acc0d5ce49923e4465e9038c55bb687
Patrick <patrykrzucidlo1@gmail.com>
2021-04-12 12:58:48 +0200

    Fix exports of decorators in index.ts

--- lib/decorators/data.decorator.ts
+++ lib/decorators/data.decorator.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import 'reflect-metadata';
-
 import createParamDecorator from './create-param-decorator';
 
 const Data = createParamDecorator((options, eventData) => {
--- lib/index.ts
+++ lib/index.ts
@@ -2,15 +2,15 @@ export * from './client/dove.client';
 export { default as DoveClient } from './client/dove.client';
 export * from './constants/dove-mode.enum';
 export { default as DoveMode } from './constants/dove-mode.enum';
-export * from './decorators/message-pattern.decorator';
 
+export * from './decorators/message-pattern.decorator';
 export { default as MessagePattern } from './decorators/message-pattern.decorator';
 export * from './decorators/data.decorator';
-export { default as DataDecorator } from './decorators/data.decorator';
+export { default as Data } from './decorators/data.decorator';
+export * from './decorators/event-id.decorator';
+export { default as EventId } from './decorators/event-id.decorator';
 export * from './decorators/create-param-decorator';
 export { default as createParamDecorator } from './decorators/create-param-decorator';
-export * from './decorators/event-id.decorator';
-export { default as EventIdDecorator } from './decorators/event-id.decorator';
 
 export * from './dove';
 export { default as Dove } from './dove';
