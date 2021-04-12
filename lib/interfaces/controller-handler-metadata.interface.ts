8996effe2501d6aea310736bf6af29fa5be920f9
Patrick <patrykrzucidlo1@gmail.com>
2021-04-12 21:42:44 +0200

    Add middleware property to ControllerHandlerMetadata

--- lib/interfaces/controller-handler-metadata.interface.ts
+++ lib/interfaces/controller-handler-metadata.interface.ts
@@ -1,9 +1,11 @@
 import { MessageHandler } from '../types/message-handler.type';
 import { HandlerParamMetadata } from './handler-param-metadata.interface';
+import { Middleware } from './middleware.interface';
 
 export interface ControllerHandlerMetadata {
   // eslint-disable-next-line @typescript-eslint/ban-types
   controller: Function;
   handler: MessageHandler;
+  middleware: Middleware[];
   paramsMetadata: HandlerParamMetadata<any>[];
 }
