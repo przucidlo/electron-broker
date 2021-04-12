1cb8bbef3803c1553227927fd0caf3ee652be847
Patrick <patrykrzucidlo1@gmail.com>
2021-04-12 13:14:57 +0200

    Change type of Symbols.InternalMiddlware to ClassType<Middleware>

--- lib/container/composers/middleware.composer.ts
+++ lib/container/composers/middleware.composer.ts
@@ -1,20 +1,15 @@
+import { INTERNAL_MIDDLEWARE_ORDER } from '../../constants/internal-middleware-order';
 import { Symbols } from '../../constants/symbols';
-import { Middleware } from '../../interfaces/middleware.interface';
-import { ResultBroadcastMiddleware } from '../../middleware/internal/result-broadcast.middleware';
 import { ContainerConfiguarableComposer } from '../abstract/container-configurable-composer';
 
 export class MiddlewareComposer extends ContainerConfiguarableComposer {
-  private static readonly INTERNAL_MIDDLEWARE_ORDER: (new (...any: any[]) => Middleware)[] = [
-    ResultBroadcastMiddleware,
-  ];
-
   public compose(): void {
     this.bindInternalMiddleware();
   }
 
   private bindInternalMiddleware() {
-    for (const middleware of MiddlewareComposer.INTERNAL_MIDDLEWARE_ORDER) {
-      this.container.bind(Symbols.InternalMiddleware).to(middleware).inRequestScope();
+    for (const middleware of INTERNAL_MIDDLEWARE_ORDER) {
+      this.container.bind(Symbols.InternalMiddleware).toConstantValue(middleware);
     }
   }
 }
