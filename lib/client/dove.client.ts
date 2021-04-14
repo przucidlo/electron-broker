bff4b2f70cd1a63ae20692c096e4f3a662a67010
Patrick <patrykrzucidlo1@gmail.com>
2021-04-14 18:02:23 +0200

    Implement Middleware feature in DoveClient

--- lib/client/dove.client.ts
+++ lib/client/dove.client.ts
@@ -1,26 +1,53 @@
 import { inject, injectable } from 'inversify';
+import { Middleware } from '..';
 import { Symbols } from '../constants/symbols';
 import { BrokerEventConverter } from '../helpers/broker-event.converter';
 import { BrokerEvent } from '../interfaces/broker-event.interface';
 import { IpcTransport } from '../interfaces/ipc-transport.interface';
+import { ClassType } from '../types/class.type';
+import { ClientExecutionContextFactory } from '../types/client-execution-context-factory.type';
+import { MiddlewareExecutorFactory } from '../types/middleware-executor-factory.type';
 import { BrokerEventSubscriber } from './event-subscriber/broker-event-subscriber';
 import { BrokerResponseListener } from './response-listener/broker-response-listener';
+
 @injectable()
 export default class DoveClient {
-  constructor(@inject(Symbols.IpcTransport) private ipcTransport: IpcTransport) {}
+  private middleware: (ClassType<Middleware> | Middleware)[];
+
+  constructor(
+    @inject(Symbols.IpcTransport) private ipcTransport: IpcTransport,
+    @inject(Symbols.MiddlewareExecutorFactory) private middlewareExecutorFactory: MiddlewareExecutorFactory,
+    @inject(Symbols.ClientExecutionContextFactory) private executionContextFactory: ClientExecutionContextFactory,
+  ) {
+    this.middleware = [];
+  }
+
+  public subscribe<T>(pattern: string, listener: (data: T) => void): BrokerEventSubscriber {
+    return new BrokerEventSubscriber(pattern, (event) => {
+      listener(<T>event.data);
+    });
+  }
 
   public send(pattern: string, data: unknown): void {
     const brokerEvent: BrokerEvent = BrokerEventConverter.createOrConvert(pattern, data);
+    const middlewareExecutor = this.middlewareExecutorFactory(this.middleware);
+    const executionContext = this.executionContextFactory(brokerEvent);
 
-    this.ipcTransport.send(brokerEvent.pattern, brokerEvent.data);
+    middlewareExecutor.executeWithoutResponse(executionContext, () => {
+      this.ipcTransport.send(brokerEvent.pattern, brokerEvent.data);
+    });
   }
 
   public async invoke<T>(pattern: string, data: unknown): Promise<T> {
     const brokerEvent: BrokerEvent = BrokerEventConverter.createOrConvert(pattern, data);
+    const middlewareExecutor = this.middlewareExecutorFactory(this.middleware);
+    const executionContext = this.executionContextFactory(brokerEvent);
 
-    this.ipcTransport.send(brokerEvent.pattern, brokerEvent.data);
+    return <T>await middlewareExecutor.execute(executionContext, async () => {
+      this.ipcTransport.send(brokerEvent.pattern, brokerEvent.data);
 
-    return await this.listenForResponse(brokerEvent);
+      return await this.listenForResponse(brokerEvent);
+    });
   }
 
   private async listenForResponse<T>(brokerEvent: BrokerEvent): Promise<T> {
@@ -29,9 +56,7 @@ export default class DoveClient {
     return <T>(await brokerResponseListener.listen()).data;
   }
 
-  public subscribe<T>(pattern: string, listener: (data: T) => void): BrokerEventSubscriber {
-    return new BrokerEventSubscriber(pattern, (event) => {
-      listener(<T>event.data);
-    });
+  public setMiddleware(middlewares: (ClassType<Middleware> | Middleware)[]): void {
+    this.middleware = middlewares;
   }
 }
