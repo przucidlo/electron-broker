bb246c0d43b3529d0d20c451c69097b2448b4498
Patrick <patrykrzucidlo1@gmail.com>
2021-04-14 17:42:43 +0200

    Fix wrong param type in ClientExecutionContextFactory

--- lib/container/composers/factory/execution-context-factory.composer.ts
+++ lib/container/composers/factory/execution-context-factory.composer.ts
@@ -3,6 +3,7 @@ import { BrokerEventData } from '../../../interfaces/broker-event-data.interface
 import { ControllerHandlerMetadata } from '../../../interfaces/controller-handler-metadata.interface';
 import { ExecutionContext } from '../../../controllers/execution-context';
 import { ContainerConfiguarableComposer } from '../../abstract/container-configurable-composer';
+import { ClientExecutionContextFactory } from '../../../types/client-execution-context-factory.type';
 
 export class ExecutionContextFactoryComposer extends ContainerConfiguarableComposer {
   public compose(): void {
@@ -21,12 +22,14 @@ export class ExecutionContextFactoryComposer extends ContainerConfiguarableCompo
   }
 
   private bindClientExecutionContextFactory() {
-    this.container.bind(Symbols.ClientExecutionContextFactory).toFactory(() => {
-      return (brokerEvent) => {
-        const executionContext = new ExecutionContext(undefined, undefined, brokerEvent);
+    this.container.bind(Symbols.ClientExecutionContextFactory).toFactory(
+      (): ClientExecutionContextFactory => {
+        return (brokerEvent) => {
+          const executionContext = new ExecutionContext(undefined, undefined, brokerEvent.data);
 
-        return executionContext;
-      };
-    });
+          return executionContext;
+        };
+      },
+    );
   }
 }
