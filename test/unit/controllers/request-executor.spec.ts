3b12579d4e7cf9a8cc6de17fcb2e3f7a1d26432f
Patrick <patrykrzucidlo1@gmail.com>
2021-04-16 12:32:33 +0200

    Fix RequestExecutor tests

--- test/unit/controllers/request-executor.spec.ts
+++ test/unit/controllers/request-executor.spec.ts
@@ -3,6 +3,7 @@ import { HandlerParamsMapper } from '../../../lib/controllers/handler-params-map
 import { RequestExecutor } from '../../../lib/controllers/request-executor';
 import { ControllerHandlerMetadata } from '../../../lib/interfaces/controller-handler-metadata.interface';
 import Middleware from '../../../lib/interfaces/middleware.interface';
+import { ControllerMetadataReader } from '../../../lib/metadata-readers/controller-metadata.reader';
 import { MiddlewareExecutor } from '../../../lib/middleware/middleware-executor';
 import { getMockBrokerEventData } from '../__mocks__/get-mock-broker-event-data';
 import { getMockTestControllerMetadata, MOCK_TEST_CONTROLLER_PATTERN } from '../__mocks__/mock-test-controller';
@@ -23,11 +24,7 @@ describe('RequestExecutor', () => {
       onResponse: jest.fn(),
     };
 
-    executionContext = new ExecutionContext(
-      handlerMetadata.controller,
-      handlerMetadata.handler,
-      getMockBrokerEventData(),
-    );
+    executionContext = new ExecutionContext(handlerMetadata, getMockBrokerEventData());
 
     requestExecutor = new RequestExecutor(
       [<any>middleware],
@@ -46,7 +43,7 @@ describe('RequestExecutor', () => {
 
     it('Should execute handler', async () => {
       const mockHandler = jest.fn();
-      executionContext.getHandler = () => mockHandler;
+      handlerMetadata.handler.apply = mockHandler;
 
       await requestExecutor.executeRequest(executionContext, handlerMetadata);
 
