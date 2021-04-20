05780402f1aaf67237729544fb558057316facaa
Patrick <patrykrzucidlo1@gmail.com>
2021-04-20 16:43:29 +0200

    Write test for ResultBroadcastMiddleware

---
+++ test/unit/middleware/result-broadcast.middleware.spec.ts
@@ -0,0 +1,37 @@
+import { ExecutionContext } from '../../../lib';
+import { BROKER_EVENT } from '../../../lib/constants/channels';
+import { BrokerEventData } from '../../../lib/interfaces/broker-event-data.interface';
+import { IpcTransport } from '../../../lib/interfaces/ipc-transport.interface';
+import { ResultBroadcastMiddleware } from '../../../lib/middleware/internal/result-broadcast.middleware';
+import { getMockBrokerEventData } from '../__mocks__/get-mock-broker-event-data';
+import { getMockIpcTransport } from '../__mocks__/get-mock-ipc-transport';
+
+describe('ResultBroadcastMiddleware', () => {
+  let middleware: ResultBroadcastMiddleware;
+  let ipcTransport: IpcTransport;
+  let fakeExecutionContext: ExecutionContext;
+
+  beforeEach(() => {
+    ipcTransport = getMockIpcTransport();
+    middleware = new ResultBroadcastMiddleware(ipcTransport);
+
+    fakeExecutionContext = <ExecutionContext>{ brokerEventData: getMockBrokerEventData() };
+  });
+
+  it('Should broadcast controller response using ipcTransport.send', () => {
+    middleware.onRequest(fakeExecutionContext);
+    middleware.onResponse({});
+
+    expect(ipcTransport.send).toBeCalled();
+  });
+
+  it('Should change BrokerEventData to be a response containing controller result', () => {
+    const mockData = '123';
+    const expectedBrokerEventData: BrokerEventData = { ...getMockBrokerEventData(), type: 'RESPONSE', data: mockData };
+
+    middleware.onRequest(fakeExecutionContext);
+    middleware.onResponse(mockData);
+
+    expect(ipcTransport.send).toBeCalledWith(BROKER_EVENT, expect.objectContaining(expectedBrokerEventData));
+  });
+});
