9911e310d078dee38baf6d9b87d534b1c594ef1e
Patrick <patrykrzucidlo1@gmail.com>
2021-04-20 21:18:36 +0200

    Write test for BrokerEventFactory

---
+++ test/unit/helpers/broker-event.factory.spec.ts
@@ -0,0 +1,32 @@
+import { BrokerEventFactory } from '../../../lib/helpers/broker-event.factory';
+import { BrokerEvent } from '../../../lib/interfaces/broker-event.interface';
+import { getMockBrokerEventData } from '../__mocks__/get-mock-broker-event-data';
+
+describe('BrokerEventFactory', () => {
+  describe('createBrokerEvent', () => {
+    it('Should create new BrokerEvent object', () => {
+      const expectedBrokerEvent: BrokerEvent = {
+        type: 'REQUEST',
+        eventId: expect.any(String),
+        pattern: 'test',
+        data: {},
+      };
+
+      expect(BrokerEventFactory.createBrokerEvent('test', {})).toStrictEqual(expectedBrokerEvent);
+    });
+  });
+
+  describe('createBrokerEventAsResponse', () => {
+    it('Should merge base BrokerEvent with new data, and change its type to response', () => {
+      const brokerEvent = getMockBrokerEventData();
+
+      const expectedBrokerEvent: BrokerEvent = {
+        ...brokerEvent,
+        type: 'RESPONSE',
+        data: '123',
+      };
+
+      expect(BrokerEventFactory.createBrokerEventAsResponse(brokerEvent, '123')).toStrictEqual(expectedBrokerEvent);
+    });
+  });
+});
