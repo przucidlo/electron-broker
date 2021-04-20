e9c974e059c6749b916ea3fca341796d0d655031
Patrick <patrykrzucidlo1@gmail.com>
2021-04-20 13:32:55 +0200

    Write test for BrokerMode

--- test/unit/modes/client.mode.spec.ts
+++ test/unit/modes/client.mode.spec.ts
@@ -1,27 +1,13 @@
 import { ControllersRegistrator } from '../../../lib/controllers/controllers-registrator';
-import { RequestExecutorInjector } from '../../../lib/controllers/request-executor-injector';
-import { ControllerMetadata } from '../../../lib/interfaces/controller-metadata.interface';
-import { IpcTransport } from '../../../lib/interfaces/ipc-transport.interface';
 import { ClientMode } from '../../../lib/modes/client.mode';
-import { getMockIpcTransport } from '../__mocks__/get-mock-ipc-transport';
-import { getMockTestControllerMetadata } from '../__mocks__/mock-test-controller';
+import { getMockControllersRegistrator } from './mocks/mock-controllers-registrator';
 
 describe('ClientMode', () => {
-  let requestExecutorInjector: RequestExecutorInjector;
   let controllerRegistrator: ControllersRegistrator;
-  let controllerMetadata: ControllerMetadata;
-  let ipcTransport: IpcTransport;
-
   let clientMode: ClientMode;
 
   beforeEach(() => {
-    requestExecutorInjector = <RequestExecutorInjector>(<unknown>{ injectIntoControllers: jest.fn() });
-    ipcTransport = getMockIpcTransport();
-    controllerMetadata = getMockTestControllerMetadata();
-
-    controllerRegistrator = new ControllersRegistrator(requestExecutorInjector, ipcTransport, () => [
-      controllerMetadata,
-    ]);
+    controllerRegistrator = getMockControllersRegistrator();
 
     clientMode = new ClientMode(controllerRegistrator);
   });
