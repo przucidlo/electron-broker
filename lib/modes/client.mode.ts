222b209b5aa4f479f9ce167cfa5e33f8659f1361
Patrick <patrykrzucidlo1@gmail.com>
2021-04-20 13:13:42 +0200

    Remove unused variable from ClientMode class

--- lib/modes/client.mode.ts
+++ lib/modes/client.mode.ts
@@ -1,12 +1,9 @@
 import { injectable } from 'inversify';
 import { ControllersRegistrator } from '../controllers/controllers-registrator';
-import { IpcTransport } from '../interfaces/ipc-transport.interface';
 import { ModuleMode } from '../interfaces/module-mode.interface';
 
 @injectable()
 export class ClientMode implements ModuleMode {
-  protected ipcTransport: IpcTransport;
-
   constructor(private controllersRegistrator: ControllersRegistrator) {}
 
   public start(): void {
