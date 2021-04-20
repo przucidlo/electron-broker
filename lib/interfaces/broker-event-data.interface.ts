8080561c66065dcb0a14bd4800b07f19f0b8503f
Patrick <patrykrzucidlo1@gmail.com>
2021-04-20 21:00:22 +0200

    Remove unused isBrokerEventData type-guard

--- lib/interfaces/broker-event-data.interface.ts
+++ lib/interfaces/broker-event-data.interface.ts
@@ -4,8 +4,3 @@ export interface BrokerEvent {
   pattern: string;
   data: unknown;
 }
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export function isBrokerEventData(arg: any): arg is BrokerEvent {
-  return arg.eventId !== undefined;
-}
--- test/unit/type-guards/isBrokerEventData.spec.ts
+++
@@ -1,8 +0,0 @@
-import { isBrokerEventData } from '../../../lib/interfaces/broker-event-data.interface';
-import { getMockBrokerEventData } from '../__mocks__/get-mock-broker-event-data';
-
-describe('isBrokerEventData', () => {
-  it('Should return true if provided object is type of brokerEvent', () => {
-    expect(isBrokerEventData(getMockBrokerEventData())).toBe(true);
-  });
-});
