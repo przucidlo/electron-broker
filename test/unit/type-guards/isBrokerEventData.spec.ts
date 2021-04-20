2168123e51986ae1210ee1753df4f232da190611
Patrick <patrykrzucidlo1@gmail.com>
2021-04-20 16:49:54 +0200

    Write test for isBrokerEventData type-guard

---
+++ test/unit/type-guards/isBrokerEventData.spec.ts
@@ -0,0 +1,8 @@
+import { isBrokerEventData } from '../../../lib/interfaces/broker-event-data.interface';
+import { getMockBrokerEventData } from '../__mocks__/get-mock-broker-event-data';
+
+describe('isBrokerEventData', () => {
+  it('Should return true if provided object is type of BrokerEventData', () => {
+    expect(isBrokerEventData(getMockBrokerEventData())).toBe(true);
+  });
+});
