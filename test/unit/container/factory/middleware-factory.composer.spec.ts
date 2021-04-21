bd76f96d31731bf91adedf8bde534061a1eda1a5
Patrick <patrykrzucidlo1@gmail.com>
2021-04-21 14:13:09 +0200

    Write test for MiddlewareFactoryComposer

---
+++ test/unit/container/factory/middleware-factory.composer.spec.ts
@@ -0,0 +1,46 @@
+import { Container } from 'inversify';
+import { Symbols } from '../../../../lib/constants/symbols';
+import { MiddlewareFactory } from '../../../../lib/types/middleware-factory.type';
+import { MockMiddleware } from '../../__mocks__/mock-middleware';
+import { getMockContainerWithDependencies } from '../mock/get-mock-container-with-dependencies';
+
+describe('MiddlewareFactoryComposer', () => {
+  let container: Container;
+  let middlewareFactory: MiddlewareFactory;
+
+  beforeEach(() => {
+    container = getMockContainerWithDependencies();
+
+    middlewareFactory = container.get(Symbols.MiddlewareFactory);
+  });
+
+  describe('MiddlewareFactory should: ', () => {
+    it('Should return object deepClone if middleware a type of object', () => {
+      const testMiddleware = new MockMiddleware();
+
+      expect(middlewareFactory(testMiddleware)).not.toBe(testMiddleware);
+    });
+
+    it('If middleware is injectable, should fetch it from container', () => {
+      container.bind(MockMiddleware).to(MockMiddleware);
+      const spy = jest.spyOn(container, 'get');
+
+      middlewareFactory(MockMiddleware);
+
+      expect(spy).toBeCalled();
+    });
+
+    it('Middleware object created by DI library, should always be a new instance', () => {
+      container.bind(MockMiddleware).to(MockMiddleware);
+
+      const instanceA = middlewareFactory(MockMiddleware);
+      const instanceB = middlewareFactory(MockMiddleware);
+
+      expect(instanceA).not.toBe(instanceB);
+    });
+
+    it('If middleware is not injectable and its not an instance of its class, factory should fallback to Object.create', () => {
+      expect(middlewareFactory(MockMiddleware)).toEqual(MockMiddleware.prototype);
+    });
+  });
+});
