b6d22058624676bf918b766fd078372d3ec6df36
Patrick <patrykrzucidlo1@gmail.com>
2021-04-21 15:19:04 +0200

    Write test for ControllersMetadataFactoryComposer

---
+++ test/unit/container/factory/controllers-metadata-factory.composer.spec.ts
@@ -0,0 +1,45 @@
+import { Container } from 'inversify';
+import { DoveMode } from '../../../../lib';
+import { Symbols } from '../../../../lib/constants/symbols';
+import { ControllersMetadataFactory } from '../../../../lib/types/controllers-metadata-factory.type';
+import { getMockTestControllerMetadata, MockTestController } from '../../__mocks__/mock-test-controller';
+import { getMockContainerWithDependencies } from '../mock/get-mock-container-with-dependencies';
+
+describe('ControllersMetadataFactoryComposer', () => {
+  let controllersMetadataFactory: ControllersMetadataFactory;
+  let container: Container;
+
+  beforeEach(() => {
+    container = getMockContainerWithDependencies({
+      mode: DoveMode.PROCESS,
+      options: {},
+      controllers: [new MockTestController()],
+    });
+
+    controllersMetadataFactory = container.get(Symbols.ControllersMetadataFactory);
+  });
+
+  describe('ControllersMetadataFactory', () => {
+    it('Should return controllers metadata', () => {
+      const expectedControllersMetadata = [getMockTestControllerMetadata()];
+
+      expect(controllersMetadataFactory()).toStrictEqual(expectedControllersMetadata);
+    });
+
+    it('If controller is marked as injectable, it should be initialized using DI', () => {
+      container = getMockContainerWithDependencies({
+        mode: DoveMode.PROCESS,
+        options: {},
+        controllers: [MockTestController],
+      });
+      container.bind(MockTestController).toConstantValue(new MockTestController());
+      controllersMetadataFactory = container.get(Symbols.ControllersMetadataFactory);
+
+      const spy = jest.spyOn(container, 'get');
+
+      controllersMetadataFactory();
+
+      expect(spy).toBeCalled();
+    });
+  });
+});
