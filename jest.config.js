ade122979f3c5fa5ded6ce5da0a8c057298fc67e
Patrick <patrykrzucidlo1@gmail.com>
2021-04-22 16:25:27 +0200

    Configure jest to mock electron with electron-mock.ts file

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['./test/unit/jest-setup.ts'],
+  moduleNameMapper: {
+    'electron': '<rootDir>/test/unit/__mocks__/electron-mock.ts',
+  }
 };
---
+++ test/unit/__mocks__/electron-mock.ts
@@ -0,0 +1,24 @@
+import { IpcMain, IpcRenderer } from 'electron';
+
+export let ipcRenderer: Partial<IpcRenderer> = {
+  on: jest.fn(),
+  send: jest.fn(),
+  removeListener: jest.fn(),
+};
+
+export let ipcMain: Partial<IpcMain> = {
+  on: jest.fn(),
+  removeListener: jest.fn(),
+};
+
+export function clearElectronMock(): void {
+  ipcRenderer = {
+    on: jest.fn(),
+    send: jest.fn(),
+    removeListener: jest.fn(),
+  };
+  ipcMain = {
+    on: jest.fn(),
+    removeListener: jest.fn(),
+  };
+}
