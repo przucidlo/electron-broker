63d20dafa8aa92f2fa0b33b66d12d4feef924245
Patrick <patrykrzucidlo1@gmail.com>
2021-04-22 17:50:51 +0200

    Write test for BrokerEventSubscriber

--- test/unit/client/__mocks__/get-mock-listener-adapter.ts
+++ test/unit/client/__mocks__/get-mock-listener-adapter.ts
@@ -1,3 +1,5 @@
-export function getMockListenerAdapter() {
+import { ListenerAdapter } from '../../../../lib/client/listener-adapter/listener-adapter.interface';
+
+export function getMockListenerAdapter(): ListenerAdapter {
   return { listen: jest.fn(), removeListener: jest.fn() };
 }
