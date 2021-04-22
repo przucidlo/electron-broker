501a56fd59ba5ae85a8efdd07a0847019ff3af48
Patrick <patrykrzucidlo1@gmail.com>
2021-04-22 16:25:36 +0200

    Write test for BrokerListener

---
+++ test/unit/client/adapters/broker-listener.spec.ts
@@ -0,0 +1,46 @@
+import { BrokerListener } from '../../../../lib/client/listener-adapter/adapters/broker-listener';
+import { clearElectronMock, ipcMain } from '../../__mocks__/electron-mock';
+
+describe('BrokerListener', () => {
+  let brokerListener: BrokerListener;
+
+  beforeEach(() => {
+    clearElectronMock();
+
+    brokerListener = new BrokerListener();
+  });
+
+  describe('listen', () => {
+    it('Should call ipcMain.on for provided pattern and listener', () => {
+      const pattern = 'test';
+
+      brokerListener.listen(pattern, () => ({}));
+
+      expect(ipcMain.on).toBeCalledWith(pattern, expect.any(Function));
+    });
+
+    it('Should call provided listener when it receives a message', () => {
+      const pattern = 'test';
+      const listener = jest.fn();
+
+      brokerListener.listen(pattern, listener);
+
+      const wrappedListener = (<jest.Mock>(<unknown>ipcMain.on)).mock.calls[0][1];
+      wrappedListener();
+
+      expect(listener).toBeCalled();
+    });
+  });
+
+  describe('removeListener', () => {
+    it('Should call ipcMain.removeListener to remove listener', () => {
+      const pattern = 'test';
+
+      brokerListener.listen(pattern, () => ({}));
+
+      brokerListener.removeListener();
+
+      expect(ipcMain.removeListener).toBeCalledWith(pattern, expect.any(Function));
+    });
+  });
+});
