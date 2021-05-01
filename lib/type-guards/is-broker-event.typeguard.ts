85e72ade0b5ba2af4b6692248f32a146df3c257f
Patrick <patrykrzucidlo1@gmail.com>
2021-05-01 18:48:50 +0200

    Check if target is undefined in isBrokerEvent

--- lib/type-guards/is-broker-event.typeguard.ts
+++ lib/type-guards/is-broker-event.typeguard.ts
@@ -1,5 +1,5 @@
 import { BrokerEvent } from '../interfaces/broker-event.interface';
 
 export function isBrokerEvent(target: any): target is BrokerEvent {
-  return target.type !== undefined && target.eventId !== undefined && target.pattern !== undefined;
+  return target && target.type !== undefined && target.eventId !== undefined && target.pattern !== undefined;
 }
