d24f07e4d230de8c64cc739b8b3d1f5dfa9221e6
Patrick <patrykrzucidlo1@gmail.com>
2021-09-05 14:50:11 +0200

    Add property name to SerializedError interface

--- lib/interfaces/serialized-error.interface.ts
+++ lib/interfaces/serialized-error.interface.ts
@@ -2,6 +2,7 @@ import { BROKER_EXCEPTION_MARKER } from '../constants/exceptions';
 
 export interface SerializedError {
   [BROKER_EXCEPTION_MARKER]: boolean;
-  stack: string;
+  name: string;
+  stack?: string;
   message: string;
 }
