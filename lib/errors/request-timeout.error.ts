ba7420551d1a8614d6e0c2df199c480f10e90605
Patrick <patrykrzucidlo1@gmail.com>
2021-09-16 22:36:51 +0200

    Set name of RequestTimeoutError

--- lib/errors/request-timeout.error.ts
+++ lib/errors/request-timeout.error.ts
@@ -2,6 +2,8 @@ export class RequestTimeoutError extends Error {
   constructor() {
     super('Request timeout error.');
 
+    this.name = 'RequestTimeoutError';
+
     Object.setPrototypeOf(this, RequestTimeoutError.prototype);
   }
 }
