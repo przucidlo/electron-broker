27714d4611ba26f63e0e4105f6852e679fd78c34
Patrick <patrykrzucidlo1@gmail.com>
2021-09-25 20:03:26 +0200

    Add secure property to ClientConfig options

--- lib/interfaces/options/client-config.interface.ts
+++ lib/interfaces/options/client-config.interface.ts
@@ -2,4 +2,18 @@ import { CommonConfig } from './common-config.interface';
 
 export interface ClientConfig extends CommonConfig {
   mode: 'CLIENT';
+  options: {
+    /**
+     * Uses exposed methods of ipcRenderer object from preload.js file.
+     *
+     * Setting it to false will result in falling back to use the ipcRenderer
+     * object directly, which won't work if your BrowserWindow doesn't have
+     * nodeIntegration enabled and contextIsolation disabled.
+     *
+     * (See the documentation to read about setting up the preload.js file)
+     *
+     * @default true
+     */
+    secure?: boolean;
+  };
 }
