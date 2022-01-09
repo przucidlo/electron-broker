09ac644560a541e21d330de89d4a576244f9612b
Patrick <patrykrzucidlo1@gmail.com>
2022-01-09 21:56:24 +0100

    Enforce ClientConfig interface on BrokerRenderer class

--- lib/renderer/broker-renderer.ts
+++ lib/renderer/broker-renderer.ts
@@ -1,3 +1,8 @@
 import { Broker } from '..';
+import { ClientConfig } from '../core/interfaces/options/client-config.interface';
 
-export default class BrokerRenderer extends Broker {}
+export default class BrokerRenderer extends Broker {
+  constructor(config: ClientConfig) {
+    super(config);
+  }
+}
