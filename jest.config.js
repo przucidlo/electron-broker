a41a3906f1fc9ff744964c5561f76ecb79f114ae
Patrick <patrykrzucidlo1@gmail.com>
2024-06-06 23:24:07 +0200

    fix: ci

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['./test/unit/jest-setup.ts'],
+  roots: ['./test/unit'],
   moduleNameMapper: {
-    'electron': '<rootDir>/test/unit/__mocks__/electron-mock.ts',
-  }
+    electron: '<rootDir>/test/unit/__mocks__/electron-mock.ts',
+  },
 };
